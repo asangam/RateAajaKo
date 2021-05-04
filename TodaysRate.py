2fe4e670a1b49efd8e6850c3d5fbb8d70d47985c
sangam Adhikari <san.adh2010@gmail.com>
2021-05-04 19:39:40 +1000

    Added NME and bunch of fixes

--- TodaysRate.py
+++ TodaysRate.py
@@ -3,6 +3,7 @@ import bs4
 
 siteNK='https://www.nepalikaam.com'
 siteNR='https://www.namasteremittance.com.au'
+siteNME='https://nepalmoneyexpress.com.au/'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
@@ -11,16 +12,20 @@ headers = {
 
 resNK = requests.get(siteNK)
 resNR = requests.get(siteNR,headers=headers)
+resNME = requests.get(siteNME,headers=headers)
 
 
 #type(res)
 
 resNK.text
 resNR.text
+resNME.text
 
 
 soupNK = bs4.BeautifulSoup(resNK.text,'html.parser')
 soupNR = bs4.BeautifulSoup(resNR.text,'html.parser')
+soupNME = bs4.BeautifulSoup(resNME.text,'html.parser')
+
 
 
 #type(soup)
@@ -41,5 +46,10 @@ day = spaceData[142:168]
 
 formattedNR = dataNR[0].getText()
 
+# Because NME displays custom tag
+for rate in soupNME.find_all("home"):
+    formattedNME=(rate["exchange_rate"])
+
 print("NK Services " + finalRate+" "+day + " -> $10 Charge")
 print("Namaste Remittance " + formattedNR + " -> $5 Charge")
+print("Nepal Money Express: " + formattedNME + " -> $5 Charge")
